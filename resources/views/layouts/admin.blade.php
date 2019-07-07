c76490d278c5bd8f7d170101203ddb76cdef0f63
Charlie Page <charliepage88@gmail.com>
2019-07-07 11:36:27 -0400

    m

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -20,12 +20,12 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 
-<body class="bg-grey-lightest font-sans leading-normal tracking-normal">
+<body class="bg-gray-200 leading-normal tracking-normal">
     <nav id="header" class="bg-white fixed w-full z-10 pin-t shadow">
         <div class="w-full container mx-auto flex flex-wrap items-center mt-0 pt-3 pb-3 md:pb-0">
             
             <div class="w-1/2 pl-2 md:pl-0">
-                <a class="text-black text-base xl:text-xl no-underline hover:no-underline font-bold"  href="#"> 
+                <a class="text-black text-base xl:text-xl no-underline hover:no-underline font-bold"  href="{{ route('admin.dashboard') }}"> 
                     <i class="fas fa-calendar-week text-orange-700 pr-3"></i>
                     <span>FunInATL Admin Dashboard</span>
                 </a>
@@ -43,13 +43,13 @@
                         <div id="userMenu" class="bg-white rounded shadow-md mt-2 absolute mt-12 pin-t pin-r min-w-full overflow-auto z-30 invisible">
                             <ul class="list-reset">
                                 <li>
-                                    <a href="{{ route('admin.dashboard') }}" class="px-4 py-2 block text-black hover:bg-grey-light no-underline hover:no-underline">
+                                    <a href="{{ route('admin.dashboard') }}" class="px-4 py-2 block text-black hover:bg-gray-300 no-underline hover:no-underline">
                                         Dashboard
                                     </a>
                                 </li>
-                                <li><hr class="border-t mx-2 border-grey-light"></li>
+                                <li><hr class="border-t mx-2 border-gray-300"></li>
                                 <li>
-                                    <a href="{{ route('logout') }}" class="px-4 py-2 block text-black hover:bg-grey-light no-underline hover:no-underline">
+                                    <a href="{{ route('logout') }}" class="px-4 py-2 block text-black hover:bg-gray-300 no-underline hover:no-underline">
                                         Logout
                                     </a>
                                 </li>
@@ -58,7 +58,7 @@
                     </div>
 
                     <div class="block lg:hidden pr-4">
-                        <button id="nav-toggle" class="flex items-center px-3 py-2 border rounded text-grey border-grey-dark hover:text-black hover:border-teal appearance-none focus:outline-none">
+                        <button id="nav-toggle" class="flex items-center px-3 py-2 border rounded text-gray border-gray-600 hover:text-black hover:border-teal appearance-none focus:outline-none">
                             <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                 <title>Menu</title>
                                 <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/>
@@ -73,49 +73,49 @@
 
                 <ul class="list-reset lg:flex flex-1 items-center px-4 md:px-0">
                     <li class="mr-6 my-2 md:my-0">
-                        <a href="{{ route('admin.dashboard') }}" class="block py-1 md:py-3 pl-1 align-middle no-underline hover:text-black border-b-2 <?php echo ($routeName === 'admin.dashboard' ? 'text-orange-dark border-orange-dark hover:border-orange-dark' : 'text-grey border-white hover:border-purple') ?>">
+                        <a href="{{ route('admin.dashboard') }}" class="block py-1 md:py-3 pl-1 align-middle no-underline hover:text-black border-b-2 <?php echo ($routeName === 'admin.dashboard' ? 'text-orange-600 border-orange-600 hover:border-orange-600' : 'text-gray border-white hover:border-purple') ?>">
                             <i class="fas fa-home fa-fw mr-1"></i>
                             <span class="pb-1 md:pb-0 text-sm">Dashboard</span>
                         </a>
                     </li>
 
                     <li class="mr-6 my-2 md:my-0">
-                        <a href="{{ route('admin.events.index') }}" class="block py-1 md:py-3 pl-1 align-middle no-underline hover:text-black border-b-2 <?php echo (strstr($routeName, 'admin.events') ? 'text-orange-dark border-orange-dark hover:border-orange-dark' : 'text-grey border-white hover:border-purple') ?>">
+                        <a href="{{ route('admin.events.index') }}" class="block py-1 md:py-3 pl-1 align-middle no-underline hover:text-black border-b-2 <?php echo (strstr($routeName, 'admin.events') ? 'text-orange-600 border-orange-600 hover:border-orange-600' : 'text-gray border-white hover:border-purple') ?>">
                             <i class="fa fa-calendar-alt fa-fw mr-1"></i>
                             <span class="pb-1 md:pb-0 text-sm">Events</span>
                         </a>
                     </li>
 
                     <li class="mr-6 my-2 md:my-0">
-                        <a href="{{ route('admin.locations.index') }}" class="block py-1 md:py-3 pl-1 align-middle no-underline hover:text-black border-b-2 <?php echo (strstr($routeName, 'admin.locations') ? 'text-orange-dark border-orange-dark hover:border-orange-dark' : 'text-grey border-white hover:border-purple') ?>">
+                        <a href="{{ route('admin.locations.index') }}" class="block py-1 md:py-3 pl-1 align-middle no-underline hover:text-black border-b-2 <?php echo (strstr($routeName, 'admin.locations') ? 'text-orange-600 border-orange-600 hover:border-orange-600' : 'text-gray border-white hover:border-purple') ?>">
                             <i class="fas fa-map fa-fw mr-1"></i>
                             <span class="pb-1 md:pb-0 text-sm">Locations</span>
                         </a>
                     </li>
 
                     <li class="mr-6 my-2 md:my-0">
-                        <a href="{{ route('admin.providers.index') }}" class="block py-1 md:py-3 pl-1 align-middle no-underline hover:text-black border-b-2 <?php echo (strstr($routeName, 'admin.providers') ? 'text-orange-dark border-orange-dark hover:border-orange-dark' : 'text-grey border-white hover:border-purple') ?>">
+                        <a href="{{ route('admin.providers.index') }}" class="block py-1 md:py-3 pl-1 align-middle no-underline hover:text-black border-b-2 <?php echo (strstr($routeName, 'admin.providers') ? 'text-orange-600 border-orange-600 hover:border-orange-600' : 'text-gray border-white hover:border-purple') ?>">
                             <i class="fa fa-cogs fa-fw mr-1"></i>
                             <span class="pb-1 md:pb-0 text-sm">Providers</span>
                         </a>
                     </li>
 
                     <li class="mr-6 my-2 md:my-0">
-                        <a href="{{ route('admin.categories.index') }}" class="block py-1 md:py-3 pl-1 align-middle no-underline hover:text-black border-b-2 <?php echo (strstr($routeName, 'admin.categories') ? 'text-orange-dark border-orange-dark hover:border-orange-dark' : 'text-grey border-white hover:border-purple') ?>">
+                        <a href="{{ route('admin.categories.index') }}" class="block py-1 md:py-3 pl-1 align-middle no-underline hover:text-black border-b-2 <?php echo (strstr($routeName, 'admin.categories') ? 'text-orange-600 border-orange-600 hover:border-orange-600' : 'text-gray border-white hover:border-purple') ?>">
                             <i class="fas fa-tasks fa-fw mr-1"></i>
                             <span class="pb-1 md:pb-0 text-sm">Categories</span>
                         </a>
                     </li>
 
                     <li class="mr-6 my-2 md:my-0">
-                        <a href="{{ route('admin.users.index') }}" class="block py-1 md:py-3 pl-1 align-middle no-underline hover:text-black border-b-2 <?php echo (strstr($routeName, 'admin.users') ? 'text-orange-dark border-orange-dark hover:border-orange-dark' : 'text-grey border-white hover:border-purple') ?>">
+                        <a href="{{ route('admin.users.index') }}" class="block py-1 md:py-3 pl-1 align-middle no-underline hover:text-black border-b-2 <?php echo (strstr($routeName, 'admin.users') ? 'text-orange-600 border-orange-600 hover:border-orange-600' : 'text-gray border-white hover:border-purple') ?>">
                             <i class="fa fa-users fa-fw mr-1"></i>
                             <span class="pb-1 md:pb-0 text-sm">Users</span>
                         </a>
                     </li>
 
                     <li class="mr-6 my-2 md:my-0">
-                        <a href="{{ route('admin.tags.index') }}" class="block py-1 md:py-3 pl-1 align-middle no-underline hover:text-black border-b-2 <?php echo (strstr($routeName, 'admin.tags') ? 'text-orange-dark border-orange-dark hover:border-orange-dark' : 'text-grey border-white hover:border-purple') ?>">
+                        <a href="{{ route('admin.tags.index') }}" class="block py-1 md:py-3 pl-1 align-middle no-underline hover:text-black border-b-2 <?php echo (strstr($routeName, 'admin.tags') ? 'text-orange-600 border-orange-600 hover:border-orange-600' : 'text-gray border-white hover:border-purple') ?>">
                             <i class="fa fa-tags fa-fw mr-1"></i>
                             <span class="pb-1 md:pb-0 text-sm">Tags</span>
                         </a>
@@ -123,9 +123,9 @@
                 </ul>
                 
                 <div class="relative pull-right pl-4 pr-4 md:pr-0">
-                    <input type="search" placeholder="Search" class="w-full bg-grey-lightest text-sm text-grey-darkest transition border focus:outline-none focus:border-grey-darker rounded py-1 px-2 pl-10 appearance-none leading-normal">
+                    <input type="search" placeholder="Search" class="w-full bg-gray-300 text-sm text-gray-700 transition border focus:outline-none focus:border-gray-600er rounded py-1 px-2 pl-10 appearance-none leading-normal">
                     <div class="absolute search-icon" style="top: 0.375rem;left: 1.75rem;">
-                        <svg class="fill-current pointer-events-none text-grey-darkest w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                        <svg class="fill-current pointer-events-none text-gray-700 w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                             <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"></path>
                         </svg>
                     </div>
@@ -136,7 +136,7 @@
 
     <!--Container-->
     <div class="container w-full mx-auto pt-20">
-        <div id="app" class="w-full px-4 md:px-0 md:mt-8 mb-16 text-grey-darkest leading-normal">
+        <div id="app" class="w-full px-4 md:px-0 md:mt-8 mb-16 text-gray-700 leading-normal">
             @if(session()->has('is-error'))
                 <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-8" role="alert">
                   <strong class="font-bold">Error</strong>
@@ -162,9 +162,9 @@
     </div> 
     <!--/container-->
   
-    <footer class="bg-white border-t border-grey-light shadow"> 
+    <footer class="bg-white border-t border-gray-300 shadow"> 
         <div class="container mx-auto flex py-8 mb-4">
-            <p class="w-full text-grey-dark text-sm text-center">
+            <p class="w-full text-gray-600 text-sm text-center">
               Copyright &copy; {{ date('Y') }} Charles Page. 
             </p>
         </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,45 +19,64 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body class="bg-grey-lightest font-sans leading-normal tracking-normal">
+<body class="bg-gray-200 leading-normal tracking-normal">
     <nav id="header" class="bg-white fixed w-full z-10 pin-t shadow">
-        <div class="w-full container mx-auto flex flex-wrap items-center mt-0 pt-3 pb-3 md:pb-0">
+        <div class="w-full container mx-auto flex flex-wrap items-center px-8 mt-0 pt-3 pb-3 md:pb-0">
             
             <div class="w-1/2 pl-2 md:pl-0">
-                <a class="text-black text-base xl:text-xl no-underline hover:no-underline font-bold"  href="/"> 
-                    <i class="fas fa-sun text-orange-dark pr-3"></i>
+                <a class="text-black text-base xl:text-xl no-underline hover:no-underline font-bold"  href="{{ route('admin.dashboard') }}"> 
+                    <i class="fas fa-calendar-week text-orange-700 pr-3"></i>
                     <span>FunInATL</span>
                 </a>
             </div>
 
             <div class="w-1/2 pr-0">
                 <div class="flex relative inline-block float-right">
-                    <div class="relative text-sm">
-                        <button id="userButton" class="flex items-center focus:outline-none mr-3">
-                            <span class="hidden md:inline-block">Hi Guest,</span>
-                            <i class="fas fa-angle-down fa-fw mr-3 text-black"></i>
-                        </button>
-                        
-                        <div id="userMenu" class="bg-white rounded shadow-md mt-2 absolute mt-12 pin-t pin-r min-w-full overflow-auto z-30 invisible">
-                            <ul class="list-reset">
-                                <li>
-                                    <a href="{{ route('login') }}" class="px-4 py-2 block text-black hover:bg-grey-light no-underline hover:no-underline">
-                                        Login
-                                    </a>
-                                </li>
-                                <li><hr class="border-t mx-2 border-grey-light"></li>
-                                <li>
-                                    <a href="{{ route('logout') }}" class="px-4 py-2 block text-black hover:bg-grey-light no-underline hover:no-underline">
-                                        Logout
-                                    </a>
-                                </li>
-                            </ul>
+                    @if(Auth::check())
+                        <div class="relative text-sm">
+                            <button id="userButton" class="flex items-center focus:outline-none mr-3">
+                                {{-- <img class="w-8 h-8 rounded-full mr-4" src="http://i.pravatar.cc/300" alt="Avatar of User"> --}}
+                                <span class="hidden md:inline-block">Hi, {{ Auth::user()->name }}</span>
+                                <i class="fas fa-angle-down fa-fw mr-3 text-black"></i>
+                            </button>
+                            
+                            <div id="userMenu" class="bg-white rounded shadow-md mt-2 absolute mt-12 pin-t pin-r min-w-full overflow-auto z-30 invisible">
+                                <ul class="list-reset">
+                                    <li>
+                                        <a href="{{ route('admin.dashboard') }}" class="px-4 py-2 block text-black hover:bg-gray-300 no-underline hover:no-underline">
+                                            Dashboard
+                                        </a>
+                                    </li>
+                                    <li><hr class="border-t mx-2 border-gray-300"></li>
+                                    <li>
+                                        <a href="{{ route('logout') }}" class="px-4 py-2 block text-black hover:bg-gray-300 no-underline hover:no-underline">
+                                            Logout
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
-                    </div>
-
+                    @else
+                        <div class="relative text-sm">
+                            <button id="userButton" class="flex items-center focus:outline-none mr-3">
+                                <span class="hidden md:inline-block">Hi Guest,</span>
+                                <i class="fas fa-angle-down fa-fw mr-3 text-black"></i>
+                            </button>
+                            
+                            <div id="userMenu" class="bg-white rounded shadow-md mt-2 absolute mt-12 pin-t pin-r min-w-full overflow-auto z-30 invisible">
+                                <ul class="list-reset">
+                                    <li>
+                                        <a href="{{ route('login') }}" class="px-4 py-2 block text-black hover:bg-gray-300 no-underline hover:no-underline">
+                                            Login
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    @endif
 
                     <div class="block lg:hidden pr-4">
-                        <button id="nav-toggle" class="flex items-center px-3 py-2 border rounded text-grey border-grey-dark hover:text-black hover:border-teal appearance-none focus:outline-none">
+                        <button id="nav-toggle" class="flex items-center px-3 py-2 border rounded text-gray border-gray-600 hover:text-black hover:border-teal appearance-none focus:outline-none">
                             <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
                         </button>
                     </div>
@@ -66,15 +85,15 @@
         </div>
     </nav>
 
-    <div class="container w-full mx-auto pt-20">
-        <div class="w-full px-4 md:px-0 md:mt-8 mb-16 text-grey-darkest leading-normal">
+    <div class="container w-full mx-auto px-16 pt-20">
+        <div class="w-full px-4 md:px-0 md:mt-8 mb-16 text-gray-700 leading-normal">
             @yield('content')
         </div>
     </div>
 
-    <footer class="bg-white border-t border-grey-light shadow"> 
+    <footer class="bg-white border-t border-gray-300 shadow"> 
         <div class="container mx-auto flex py-8 mb-4">
-            <p class="w-full text-grey-dark text-sm text-center">
+            <p class="w-full text-gray-600 text-sm text-center">
               Copyright &copy; {{ date('Y') }} Charles Page. 
             </p>
         </div>
