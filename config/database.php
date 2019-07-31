36d05d151e81909e5aad75c599fbd307a6fd665e
charliepage88 <charliepage88@gmail.com>
2019-07-31 20:30:08 +0000

    m

--- config/database.php
+++ config/database.php
@@ -139,10 +139,10 @@ return [
         ],
 
         'cache' => [
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', 6379),
-            'database' => env('REDIS_CACHE_DB', 8),
+            'host' => env('REDIS_CACHE_HOST', env('REDIS_HOST')),
+            'password' => env('REDIS_CACHE_PASSWORD', env('REDIS_PASSWORD', null)),
+            'port' => env('REDIS_CACHE_PORT', env('REDIS_PORT', 6379)),
+            'database' => env('REDIS_CACHE_DB', 1),
         ]
     ]
 ];
